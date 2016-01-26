c8a8e44ee98e3696ca96ca3a3b7c453fdaf40eb8
Pegah Jandaghi <jandaghi72@gmail.com>
2016-01-26 13:51:37 +0330

    2

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -3,21 +3,14 @@ import java.util.HashSet;
 
 
 public class Scheduler {
-	HashSet<Long> Visited;
 	ArrayList<String> urlQ;
 	ArrayList<String> SeenUrl;
 	
 	public Scheduler() {
-		this.Visited = new HashSet<>();
 		this.urlQ = new ArrayList<>();
 		this.SeenUrl = new ArrayList<>();
 	}
 	
-	public boolean isRepeated(long id){
-		if (Visited.contains(id))
-			return true;
-		return false;
-	}
 	
 	public String getNextUrl(){
 		if (urlQ.size() > 0)
@@ -26,19 +19,9 @@ public class Scheduler {
 			return "";
 	}
 	
-	public boolean finish(){
-		if (Visited.size() > 1000)
-			return true;
-		return false;
-	}
-	
 	public void addUrl(String url){
 		if (SeenUrl.contains(url))
 			return;
 		urlQ.add(url);
-	}
-	
-	public void VisitedId(long id){
-		Visited.add(id);
 	}
 }
