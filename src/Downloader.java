e45b40e8436f0f16a5e438d0962585e257c557ba
Pegah Jandaghi <jandaghi72@gmail.com>
2016-01-26 13:05:30 +0330

    2nd

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,4 +1,23 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class Downloader {
-
+	public static void main(String[] args){
+		
+	}
+	
+	
+	// download the html file of the given url 
+	public static void Downloader(String url, String outputName)throws IOException{
+		URL website = new URL(url);
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		FileOutputStream fos = new FileOutputStream(outputName+ ".html");
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		fos.close();
+		rbc.close();
+	}
 }
