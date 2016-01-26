843517cc6b16fc1b1b616bec89bae9eb98798d7d
Pegah Jandaghi <jandaghi72@gmail.com>
2016-01-26 13:26:43 +0330

    2Scheduler

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,11 +1,38 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 
 
 public class Scheduler {
-	HashSet<Long> quque;
+	HashSet<Long> Visited;
+	ArrayList<String> urlQ;
+	
 	public Scheduler() {
-		this.quque = new HashSet<>();
+		this.Visited = new HashSet<>();
+		this.urlQ = new ArrayList<>();
 	}
 	
-	public boolean isRepeated()
+	public boolean isRepeated(long id){
+		if (Visited.contains(id))
+			return true;
+		return false;
+	}
+	
+	public String getNextUrl(){
+		if (urlQ.size() > 0)
+			return urlQ.remove(0);
+		else
+			return "";
+	}
+	
+	public boolean finish(){
+		if (Visited.size() > 1000)
+			return true;
+		return false;
+	}
+	
+	public void addUrl(String url){
+		if (urlQ.contains(url))
+			return;
+		urlQ.add(url);
+	}
 }
