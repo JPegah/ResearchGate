88929db15283c3ae3a95a783760fbd4726d37e6b
Pegah <pegah@pegah-VPCS13AFG.(none)>
2016-01-29 09:46:11 -0500

    index created

--- src/Indexer.java
+++ src/Indexer.java
@@ -10,9 +10,15 @@ import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.log4j.BasicConfigurator;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.json.simple.JSONArray;
@@ -21,70 +27,83 @@ import org.json.simple.parser.JSONParser;
 
 import com.vividsolutions.jts.io.ParseException;
 
-
-
 public class Indexer {
 	public static void main(String[] args) {
 
 		BasicConfigurator.configure();
-		Node node = NodeBuilder.nodeBuilder()
-	    .settings(Settings.builder()
-	        .put("path.home", "/home/pegah/elasticsearch-2.1.1/")).node();
+		Node node = NodeBuilder
+				.nodeBuilder()
+				.settings(
+						Settings.builder().put("path.home",
+								"/home/pegah/elasticsearch-2.1.1/")).node();
 
 		Client client = node.client();
 		try {
-			client.admin().indices().prepareCreate("pegah_index").execute().actionGet();
+			String indexName = "pegah_ind";
+			IndicesExistsResponse res = client.admin().indices()
+					.prepareExists(indexName).execute().actionGet();
+			if (res.isExists()) {
+				DeleteIndexRequestBuilder delIdx = client.admin().indices().prepareDelete(indexName);
+				delIdx.execute().actionGet();
+			}
+
+			CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate(indexName);
+			XContentBuilder builder = XContentFactory.jsonBuilder()
+					.startObject().startObject("doc").startObject("properties")
+					.startObject("abstract").
+						field("type", "string").field("store", "yes").field("index", "not_analyzed").
+					endObject()
+					.startObject("authors").
+						field("type", "string").field("store", "yes").field("index", "not_analyzed").
+					endObject()
+					.startObject("title").
+					field("type", "string").field("store", "yes").field("index", "not_analyzed").
+					endObject().
+					startObject("pub_id").
+					field("type", "long").field("store", "yes").field("index", "not_analyzed").
+					endObject().
+					startObject("citee").
+					field("type", "long[]").field("store", "yes").field("index", "not_analyzed").
+					endObject().
+					startObject("citedBy").
+					field("type", "long[]").field("store", "yes").field("index", "not_analyzed").
+					endObject().
+					startObject("URL").
+					field("type", "string").field("store", "yes").field("index", "not_analyzed").
+					endObject().
+					
+					// more mapping
+					endObject().endObject().endObject();
+			client.admin().indices().preparePutMapping(indexName).setType("doc")
+					.setSource(builder).execute().actionGet();
+
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println("indedx was created");
 		}
-		
 
-		// on shutdown
-		System.out.println("index created");
+		Map<Long, Integer> pubToInd = new HashMap<Long, Integer>();
+
+		// node.close();
+
+	}
+	public static void addDocs(Client client, Map<Long, Integer> map){
 		File[] files = new File("papers/").listFiles();
-		for (int i = 0; i < 2; i++){
-//			Scanner sc = null;
-//			try {
-//				sc = new Scanner(files[i]);				
-//			} catch (Exception e) {
-//				System.err.println("could not read file");
-//			}
-////			System.err.println(sc.nextLine());
-//			String json = sc.nextLine();
-
-		
+		for (int i = 0; i < files.length; i++) {
 			JSONParser parser = new JSONParser();
-
 			try {
-
-				Map<String, Object> json = new HashMap<>();
-//				json.put("postDate",new Date());
-				json.put("message","trying out Elasticsearch");
-				
 				Object obj = parser.parse(new FileReader(files[i].getPath()));
 				JSONObject jsonObject = (JSONObject) obj;
-				String Abst = (String) jsonObject.get("abstract");
-				json.put("abstract",Abst);
-				System.err.println(Abst);
-//
-//				long age = (Long) jsonObject.get("age");
-//				System.out.println(age);
-//
-//				// loop array
-				JSONArray auths = (JSONArray) jsonObject.get("authors");
-				Iterator<String> iterator = auths.iterator();
-				while (iterator.hasNext()) {
-					System.out.println(iterator.next());
-				}
-				IndexResponse response = client.prepareIndex("pegah_index", "pegah")
-				        .setSource(obj)
-				        .get();
+		
+				IndexResponse response = client
+						.prepareIndex().setIndex("pegah_ind").setType("doc").setSource(jsonObject).setRefresh(true).execute().actionGet();
 				System.err.println(response.getIndex());
 				System.err.println(response.getType());
 				System.err.println(response.getId());
 				System.err.println(response.getVersion());
 				System.err.println(response.isCreated());
+				
+				//create graph for pageRank
+				
 
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -95,11 +114,6 @@ public class Indexer {
 				e.printStackTrace();
 			}
 
-		     }
-		
-		
-		System.out.println(files.length);
-//		node.close();
-
+		}
 	}
 }
